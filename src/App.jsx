f97922800024c8435681f24c1180903d8168a551
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-06-20 04:08:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Movie from './components/pages/Movie';
+import AppLayout from './components/layout/AppLayout';
+import Error from './components/pages/Error';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/about',
+          element: <About />
+        },
+        {
+          path: '/movie',
+          element: <Movie />
+        }
+      ]
+    }
+  ])
+
+  return <RouterProvider router={router} />
+}
+
+export default App
