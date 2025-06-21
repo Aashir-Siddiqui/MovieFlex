695382e14da1302bd85cdbdd07c60ab06067f334
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-06-22 02:38:21 +0500

    env file added

--- src/api/GetMoviesdata.jsx
+++ src/api/GetMoviesdata.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const GetMoviesData = async () => {
     try {
-        const response = await fetch("https://www.omdbapi.com/?apikey=8ecbb2a2&s=titanic&page=1")
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`)
         const data = response.json()
         return data
     } catch (error) {
