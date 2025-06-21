2134f745af935478ab182ab098e8f5b864379f8a
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-06-21 05:02:00 +0500

    Fetch movies data and creates movies cards

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer';
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 
 
 export default function AppLayout() {
+
+    const navigation = useNavigation()
+    console.log(navigation) 
+
+    if(navigation.state === 'loading') return <h1>Loading...</h1>
+
     return (
         <>
             <Header />
