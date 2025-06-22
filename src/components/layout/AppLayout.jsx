ad8419cd00b742762dc41e30eab5657e42b4bef8
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-06-22 22:54:09 +0500

    Update website

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer';
 import { Outlet, useNavigation } from "react-router";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 
 export default function AppLayout() {
-
     const navigation = useNavigation()
-    console.log(navigation) 
+    console.log(navigation)
 
-    if(navigation.state === 'loading') return <h1>Loading...</h1>
+    if (navigation.state === 'loading') return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <DotLottieReact
+                src="/src/assets/loader.lottie"
+                loop
+                autoplay
+                className="w-24 h-24 md:w-32 md:h-32"
+            />
+        </div>
+    )
 
     return (
         <>
