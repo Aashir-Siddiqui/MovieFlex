fb2c927bb70e886acebe87072647f3e0a2a9897c
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-06-22 22:53:48 +0500

    Update website

--- src/api/GetMoviesdata.jsx
+++ src/api/GetMoviesdata.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const GetMoviesData = async () => {
     try {
-        const response = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`)
+        const response = await fetch(` https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`)
         const data = response.json()
         return data
     } catch (error) {
